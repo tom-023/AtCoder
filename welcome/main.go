9833e8c339a8079d33d0c9ef9d11b096bb817dee
tom-023 <atsushi.watanabe@zozo.com>
2022-09-29 17:28:07 +0900

    A - Welcome to AtCoder

--- welcome/main.go
+++ welcome/main.go
@@ -34,5 +34,10 @@ import (
 )
 
 func main() {
-	fmt.Println("hoge")
+	var a, b, c int
+	var s string
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d %d", &b, &c)
+	fmt.Scanf("%s", &s)
+	fmt.Printf("%d %s\n", a+b+c, s)
 }
